5503cab06c7899a82b87778da422516548c9aea8
Mathieu Marques <mathieumarques78@gmail.com>
2018-03-26 15:20:41 +0200

    Load home.md

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import home from '../pages/home.md';
+
 class Home extends React.Component {
   render() {
     return (
-      <p>This is the homepage.</p>
+      <ReactMarkdown source={home} />
     );
   }
 }
---
+++ src/pages/home.md
@@ -0,0 +1,5 @@
+# Home
+
+Tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet
+sed euismod nisi. Dui ut ornare lectus sit amet est placerat in egestas erat
+imperdiet sed euismod!
