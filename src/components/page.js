51aeb44ed09dcb8bed19a539f133df31cb1415f9
Mathieu Marques <mathieumarques78@gmail.com>
2018-05-07 18:31:57 +0200

    Clean up <Page />

--- src/components/page.js
+++ src/components/page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Grid, Typography } from 'material-ui';
+import { Typography } from 'material-ui';
 import ReactMarkdown from 'react-markdown';
 
 function getHeading(level) {
@@ -20,18 +20,12 @@ class Page extends React.Component {
   }
 
   render() {
-
     const renderers = {
       heading: props => <Typography children={props.children} gutterBottom variant={getHeading(props.level)} />,
     };
     let { content } = this.state;
-
     return content ? (
-      <Grid container justify="center" style={{marginTop: 20}}>
-        <Grid item xs={10} sm={10} md={8} lg={6} xl={6}>
-          <Typography children={<ReactMarkdown renderers={renderers} source={content} />} component="div" />
-        </Grid>
-      </Grid>
+      <Typography children={<ReactMarkdown renderers={renderers} source={content} />} component="div" />
     ) : null;
   }
 }
