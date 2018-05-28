acba4d145cd023dbab0506db7fd184f871ba879f
Mathieu Marques <mathieumarques78@gmail.com>
2018-05-28 11:35:26 +0200

    Add row-loader to config/webpack.config.prod.js

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -212,6 +212,10 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.md$/,
+            use: 'raw-loader'
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
