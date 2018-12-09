15060f48a9b8ecd0242b413d2c5c2b850da6489d
Mathieu Marques <mathieumarques78@gmail.com>
2018-12-09 23:33:25 +0100

    Use mdi-react instead of mdi-material-ui

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import AlertOctagon from 'mdi-material-ui/AlertOctagon';
-import Baby from 'mdi-material-ui/Baby';
-import ChessKnight from 'mdi-material-ui/ChessKnight';
-import FileOutline from 'mdi-material-ui/FileOutline';
-import Puzzle from 'mdi-material-ui/Puzzle';
-import Target from 'mdi-material-ui/Target';
-import Wrench from 'mdi-material-ui/Wrench';
+import AlertOctagonIcon from 'mdi-react/AlertOctagonIcon';
+import BabyIcon from 'mdi-react/BabyIcon';
+import ChessKnightIcon from 'mdi-react/ChessKnightIcon';
+import FileOutlineIcon from 'mdi-react/FileOutlineIcon';
+import PuzzleIcon from 'mdi-react/PuzzleIcon';
+import TargetIcon from 'mdi-react/TargetIcon';
+import WrenchIcon from 'mdi-react/WrenchIcon';
 
 
 export const PUZZLES = [
@@ -22,7 +22,7 @@ export const PUZZLES = [
 export const MENU = [
 
   {
-    label: 'Chapter 1, the Fundamentals', icon: <Baby />, routes: [
+    label: 'Chapter 1, the Fundamentals', icon: <BabyIcon />, routes: [
       {text: 'Basics',                         href: '/chapters/1/basics/'},
       {text: 'Brainstorm',                     href: '/chapters/1/brainstorm/'},
       {text: 'Laboratory Maniac',              href: '/chapters/1/laboratory-maniac/'},
@@ -31,7 +31,7 @@ export const MENU = [
   },
 
   {
-    label: 'Chapter 2, Supplementary Techniques', icon: <Target />, routes: [
+    label: 'Chapter 2, Supplementary Techniques', icon: <TargetIcon />, routes: [
       {text: 'Conjurer\'s Bauble',             href: '/chapters/2/conjurers-bauble/'},
       {text: 'Shelldock Isle',                 href: '/chapters/2/shelldock-isle/'},
       {text: 'Double Doomsday Piles',          href: '/chapters/2/double-doomsday/'},
@@ -40,24 +40,24 @@ export const MENU = [
   },
 
   {
-    label: 'Chapter 3, Limitations', icon: <AlertOctagon />, routes: [
+    label: 'Chapter 3, Limitations', icon: <AlertOctagonIcon />, routes: [
       {text: 'Playing Around Surgical',        href: '/chapters/3/surgical-extraction/'},
       {text: 'CMC Limitations',                href: '/chapters/3/cmc/'},
     ]
   },
 
-  {label: 'Chapter 4, Deck Construction', icon: <Wrench />},
+  {label: 'Chapter 4, Deck Construction', icon: <WrenchIcon />},
 
-  {label: 'Chapter 5, Gameplay Strategies', icon: <ChessKnight />},
+  {label: 'Chapter 5, Gameplay Strategies', icon: <ChessKnightIcon />},
 
   {
-    label: 'Chapter 6, Puzzles', icon: <Puzzle />, routes: [
+    label: 'Chapter 6, PuzzleIcons', icon: <PuzzleIcon />, routes: [
       {text: '1',                              href: '/puzzles/1/'},
     ]
   },
 
   {
-    label: 'Postamble, Appendices', icon: <FileOutline />, routes: [
+    label: 'Postamble, Appendices', icon: <FileOutlineIcon />, routes: [
       {text: 'Primer',                         href: '/appendices/primer/'},
       {text: 'Pile Document',                  href: '/appendices/piles/'},
       {text: 'Sample Decklists',               href: '/appendices/decklists/'},
