161bdf0a1a99ce75db06a5d8a053bb1542566682
Mathieu Marques <mathieumarques78@gmail.com>
2018-12-10 15:29:03 +0100

    Add SidebarMenu component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,29 +1,21 @@
 import React from 'react';
 
 import Button from '@material-ui/core/Button';
-import Collapse from '@material-ui/core/Collapse';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import Switch from '@material-ui/core/Switch';
 import Toolbar from '@material-ui/core/Toolbar';
 import withWidth, { isWidthDown } from '@material-ui/core/withWidth';
 import withStyles from '@material-ui/core/styles/withStyles';
-
 import Link from 'react-router-dom/Link';
-import NavLink from 'react-router-dom/NavLink';
 
 import Markdown from '../Markdown';
+import SidebarMenu from '../SidebarMenu';
 import { ThemeConsumer } from '../../contexts/Theme';
-import { MENU } from '../../menu';
 
 
 const styles = theme => ({
-  activeLink: {backgroundColor: theme.palette.action.selected},
   body: {
     flexGrow: 1,
     flexShrink: 1,
@@ -39,44 +31,11 @@ class Sidebar extends React.PureComponent {
   state = {menuItems: {}};
 
   toggleMenuItem = index => () => {
-    this.setState(state => {
-      const newState = Object.assign({}, state);
-      newState.menuItems[index] = !newState.menuItems[index];
-      return newState;
-    });
+    this.setState(state => ({menuItems: {...state.menuItems, [index]: !state.menuItems[index]}}));
   };
 
   render() {
-
     const { classes, component, drawerProps, toggleDrawer, width } = this.props;
-
-    const menu = MENU.map((chapter, index) => {
-      const labels = chapter.label.split(',');
-      const routes = (chapter.routes || []).map((it, index) => (
-        <ListItem activeClassName={classes.activeLink}
-                  button
-                  children={<ListItemText primary={it.text} />}
-                  component={NavLink}
-                  dense
-                  key={index}
-                  onClick={toggleDrawer}
-                  to={it.href} />
-      ));
-      return (
-        <div key={index}>
-          <ListItem button dense disabled={!routes.length} onClick={this.toggleMenuItem(index)}>
-            <ListItemIcon children={chapter.icon} />
-            <ListItemText primary={labels[0]} secondary={labels[1]} />
-          </ListItem>
-          <Collapse in={this.state.menuItems[index]} timeout="auto">
-            <Divider />
-            <List children={routes} />
-            <Divider />
-          </Collapse>
-        </div>
-      );
-    });
-
     const drawer = (
       <ThemeConsumer>
         {theme => (
@@ -100,7 +59,9 @@ class Sidebar extends React.PureComponent {
             </Grid>
             <Divider />
             <Grid item className={classes.body}>
-              <List children={menu} component="nav" />
+              <SidebarMenu menuItems={this.state.menuItems}
+                           toggleDrawer={toggleDrawer}
+                           toggleMenuItem={this.toggleMenuItem} />
               <Divider />
               <Markdown source="notation.md" tableCellProps={{padding: 'dense'}} />
               <Markdown className={classes.padding} noPadding source="links.md" />
---
+++ src/components/SidebarMenu/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import Collapse from '@material-ui/core/Collapse';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import withStyles from '@material-ui/core/styles/withStyles';
+import NavLink from 'react-router-dom/NavLink';
+
+import { MENU } from '../../menu';
+
+
+const styles = theme => ({
+  activeLink: {backgroundColor: theme.palette.action.selected},
+});
+
+
+class SidebarMenu extends React.PureComponent {
+  render() {
+    const { classes, menuItems, toggleDrawer, toggleMenuItem } = this.props;
+    const menu = MENU.map((chapter, index) => {
+      const labels = chapter.label.split(',');
+      const routes = (chapter.routes || []).map((it, index) => (
+        <ListItem activeClassName={classes.activeLink}
+                  button
+                  children={<ListItemText primary={it.text} />}
+                  component={NavLink}
+                  dense
+                  key={index}
+                  onClick={toggleDrawer}
+                  to={it.href} />
+      ));
+      return (
+        <div key={index}>
+          <ListItem button dense disabled={!routes.length} onClick={toggleMenuItem(index)}>
+            <ListItemIcon children={chapter.icon} />
+            <ListItemText primary={labels[0]} secondary={labels[1]} />
+          </ListItem>
+          <Collapse in={menuItems[index]} timeout="auto">
+            <Divider />
+            <List children={routes} />
+            <Divider />
+          </Collapse>
+        </div>
+      );
+    });
+    return <List children={menu} component="nav" />;
+  }
+}
+
+
+export default withStyles(styles)(SidebarMenu);
