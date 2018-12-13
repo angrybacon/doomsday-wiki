a2154ee32613570adb6207cca37066a4302ebe56
Mathieu Marques <mathieumarques78@gmail.com>
2018-12-13 15:08:07 +0100

    Fix page overflow

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import Markdown from '../Markdown';
 
 
+const styles = () => ({
+  root: {overflowX: 'hidden'},
+});
+
+
 class Page extends React.PureComponent {
   render() {
-    const { match } = this.props;
-    return (
-      <Paper children={<Markdown match={match} />} component="article" />
-    );
+    const { classes, match } = this.props;
+    return <Paper children={<Markdown match={match} />} className={classes.root }component="article" />;
   }
 }
 
 
-export default Page;
+export default withStyles(styles)(Page);
