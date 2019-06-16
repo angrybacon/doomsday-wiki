b779c7ae9f087e62c12d5a4de6049b6c876d3edf
Mathieu Marques <mathieumarques78@gmail.com>
2019-06-16 16:14:22 +0200

    Clean up menu

--- src/menu.js
+++ src/menu.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 import AlertOctagonIcon from 'mdi-react/AlertOctagonIcon';
 import BabyIcon from 'mdi-react/BabyIcon';
-import ChessKnightIcon from 'mdi-react/ChessKnightIcon';
 import FileOutlineIcon from 'mdi-react/FileOutlineIcon';
 import PuzzleIcon from 'mdi-react/PuzzleIcon';
 import TargetIcon from 'mdi-react/TargetIcon';
-import WrenchIcon from 'mdi-react/WrenchIcon';
 
 
 export default [
 
   {
-    label: 'Chapter 1, the Fundamentals', icon: <BabyIcon />, routes: [
+    icon: <BabyIcon />,
+    label: 'Chapter 1, the Fundamentals',
+    routes: [
       {text: 'Basics',                         href: '/chapters/1/basics/'},
       {text: 'Brainstorm',                     href: '/chapters/1/brainstorm/'},
       {text: 'Laboratory Maniac',              href: '/chapters/1/laboratory-maniac/'},
@@ -20,32 +20,36 @@ export default [
   },
 
   {
-    label: 'Chapter 2, Supplementary Techniques', icon: <TargetIcon />, routes: [
+    icon: <TargetIcon />,
+    label: 'Chapter 2, Supplementary Techniques',
+    routes: [
       {text: 'Passing the Turn',               href: '/chapters/2/pass-the-turn/'},
-      {text: 'Conjurer\'s Bauble',             href: '/chapters/2/conjurers-bauble/'},
+      {text: "Conjurer's Bauble",              href: '/chapters/2/conjurers-bauble/'},
       {text: 'Uncommon Piles',                 href: '/chapters/2/uncommon-piles/'},
     ]
   },
 
   {
-    label: 'Chapter 3, Limitations', icon: <AlertOctagonIcon />, routes: [
+    icon: <AlertOctagonIcon />,
+    label: 'Chapter 3, Limitations',
+    routes: [
       {text: 'Playing Around Surgical',        href: '/chapters/3/surgical-extraction/'},
       {text: 'CMC Limitations',                href: '/chapters/3/cmc/'},
     ]
   },
 
-  {label: 'Chapter 4, Deck Construction', icon: <WrenchIcon />},
-
-  {label: 'Chapter 5, Gameplay Strategies', icon: <ChessKnightIcon />},
-
   {
-    label: 'Chapter 6, Puzzles', icon: <PuzzleIcon />, routes: [
+    icon: <PuzzleIcon />,
+    label: 'Chapter 4, Puzzles',
+    routes: [
       {text: '1',                              href: '/puzzles/1/'},
     ]
   },
 
   {
-    label: 'Postamble, Appendices', icon: <FileOutlineIcon />, routes: [
+    icon: <FileOutlineIcon />,
+    label: 'Postamble, Appendices',
+    routes: [
       {text: 'Primer',                         href: '/appendices/primer/'},
       {text: 'Pile Document',                  href: '/appendices/piles/'},
       {text: 'Sample Decklists',               href: '/appendices/decklists/'},
