301dbdf5eb8ee422fe93047970d9f12ffe742b13
Mathieu Marques <mathieumarques78@gmail.com>
2019-06-28 13:52:11 +0200

    Remove public/

--- src/puzzles.js
+++
@@ -1,8 +0,0 @@
-export default [
-  {
-    title: 'Tier 1', href: '/puzzles/1/', routes: [
-      {text: 'A',                              href: '/puzzles/1/a/'},
-      {text: 'B',                              href: '/puzzles/1/b/'},
-    ]
-  },
-];
