20d4101443ac337f919d399f9f2f5e6f5353abd2
Mathieu Marques <mathieumarques78@gmail.com>
2019-06-28 17:44:16 +0200

    Fix page template

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,34 +1,17 @@
-// import { graphql } from 'gatsby';
-import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import React from 'react';
 
 import Application from './Application';
+import Markdown from './Markdown';
 
 
 export default class Page extends React.PureComponent {
   render() {
-    // const { markdownRemark } = this.props.data;
-    // const { frontmatter, html } = markdownRemark;
+    const { body } = this.props.pageContext;
     return (
       <Application>
-        <Typography children="Some Markdown Content." />
-        {/* <Typography children={frontmatter.title} variant="h2" /> */}
-        {/* <Typography children={html} /> */}
+        <Paper children={<Markdown source={body} />} />
       </Application>
     );
   }
 }
-
-
-// export const query = graphql`
-//   query($path: String!) {
-//     markdownRemark(frontmatter: { path: { eq: $path } }) {
-//       frontmatter {
-//         date(formatString: "MMMM DD, YYYY")
-//         path
-//         title
-//       }
-//       html
-//     }
-//   }
-// `;
