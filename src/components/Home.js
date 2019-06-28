9ea04880ab43fb6cfc2a5f017cf92c0172f4d801
Mathieu Marques <mathieumarques78@gmail.com>
2019-06-28 14:00:11 +0200

    Add articles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,20 @@
 import { StaticQuery, graphql } from 'gatsby';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import React from 'react';
 
+import Markdown from './Markdown';
 
 
 export default class Home extends React.PureComponent {
   render() {
     const query = graphql`{
       home: file(relativePath: {eq: "home.md"}) {childMarkdownRemark {rawMarkdownBody}}
-      test: file(relativePath: {eq: "test.md"}) {childMarkdownRemark {rawMarkdownBody}}
+      articles: file(relativePath: {eq: "articles.md"}) {childMarkdownRemark {rawMarkdownBody}}
     }`;
-    return <StaticQuery query={query} render={({ home, test }) => (
+    return <StaticQuery query={query} render={({ home, articles }) => (
       <>
-        <Paper children={<Typography children={home.childMarkdownRemark.rawMarkdownBody} />} />
-        <Paper children={<Typography children={test.childMarkdownRemark.rawMarkdownBody} />} />
+        <Paper children={<Markdown source={home.childMarkdownRemark.rawMarkdownBody} />} />
+        <Paper children={<Markdown source={articles.childMarkdownRemark.rawMarkdownBody} />} />
       </>
     )} />;
   }
