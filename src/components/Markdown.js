42658cdf9d89dab1233cfd948aae40ac0b6b8291
Mathieu Marques <mathieumarques78@gmail.com>
2019-06-28 13:58:16 +0200

    Add Markdown

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,31 +1,81 @@
-import { graphql } from 'gatsby'
+import classNames from 'classnames';
+import Divider from '@material-ui/core/Divider';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+import Prettylink from './Prettylink';
+import Quote from './Quote';
+import Scryfall from './Scryfall';
 
 
-export default class Markdown extends React.PureComponent {
+const SCRYFALL_RE = /{{([^{}]+)}}/g;
+
+
+const styles = theme => ({
+  code: {
+    borderColor: theme.palette.divider,
+    borderRadius: 2,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    overflowY: 'auto',
+    padding: theme.spacing.unit,
+  },
+  divider: {
+    marginBottom: '1em',
+    marginTop: '1em',
+  },
+  gutter: {
+    marginLeft: -theme.overrides.MuiPaper.root.padding,
+    width: `calc(100% + ${theme.overrides.MuiPaper.root.padding * 2 + 1}px)`,
+  },
+  table: {overflowX: 'auto'},
+});
+
+
+class Markdown extends React.PureComponent {
   render() {
-    const { markdownRemark } = this.props.data;
-    const { frontmatter, html } = markdownRemark;
+
+    const { className, classes, gutter, source } = this.props;
+
+    const renderers = {
+      blockquote: props => <Quote children={props.children} />,
+      code: props => <pre className={classes.code}><code>{props.value}</code></pre>,
+      heading: props => (
+        <Typography children={props.children} gutterBottom variant={`h${props.level + 2}`} />
+      ),
+      link: Prettylink,
+      linkReference: Prettylink,
+      table: props => <Table children={props.children} className={gutter && classes.gutter} />,
+      tableHead: props => <TableHead children={props.children} />,
+      tableBody: props => <TableBody children={props.children} />,
+      tableRow: props => <TableRow children={props.children} />,
+      tableCell: props => <TableCell children={props.children} padding="checkbox" />,
+      text: props => {
+        const nodes = props.value.split(SCRYFALL_RE).map((it, index) => (
+          index % 2 ? <Scryfall query={it} key={index} /> : it
+        ));
+        return <React.Fragment children={nodes} />;
+      },
+      thematicBreak: () => (
+        <Divider className={classNames(classes.divider, {[classes.gutter]: gutter})} />
+      ),
+    };
+
     return (
-      <>
-        <Typography children={frontmatter.title} variant="h1" />
-        <Typography children={html} />
-      </>
+      <Typography className={className}
+                  component={ReactMarkdown}
+                  renderers={renderers}
+                  source={source} />
     );
   }
-};
+}
 
 
-export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        path
-        title
-      }
-      html
-    }
-  }
-`;
+export default withStyles(styles)(Markdown);
