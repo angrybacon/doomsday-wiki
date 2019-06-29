31600615b30cafd0f715b69e10d242eac0d1ccb3
Mathieu Marques <mathieumarques78@gmail.com>
2019-06-29 17:33:19 +0200

    Discover authors automatically

---
+++ src/components/Authors.js
@@ -0,0 +1,25 @@
+import { StaticQuery, graphql } from 'gatsby';
+import Paper from '@material-ui/core/Paper';
+import React from 'react';
+
+import Markdown from './Markdown';
+
+
+export default class Home extends React.PureComponent {
+  render() {
+    const query = graphql`{
+      allFile(
+        filter: {relativePath: {glob: "authors/**/*"}}
+        sort: {fields: name}
+      ) {edges {node {childMarkdownRemark {rawMarkdownBody}}}}
+    }`;
+    return <StaticQuery query={query} render={({ allFile }) => (
+      <>
+        {allFile.edges.map((it, index) => (
+          <Paper children={<Markdown source={it.node.childMarkdownRemark.rawMarkdownBody} />}
+                 key={index} />
+        ))}
+      </>
+    )} />;
+  }
+}
