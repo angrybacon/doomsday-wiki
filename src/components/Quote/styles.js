6df5df123a8679a547c7ad6a9573674397ec4c45
Mathieu Marques <mathieumarques78@gmail.com>
2020-01-27 00:12:03 +0100

    Fix large quotes

--- src/components/Quote/styles.js
+++ src/components/Quote/styles.js
@@ -4,15 +4,19 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(theme => ({
   root: {
     color: theme.palette.text.secondary,
-    display: 'flex',
+    display: 'block',
     margin: [[theme.spacing(2), 0]],
-    padding: [[theme.spacing(1), 0]],
+    padding: [[theme.spacing(1), 0, theme.spacing(1), theme.spacing(2)]],
+    position: 'relative',
     '&:before': {
       backgroundColor: theme.palette.secondary[theme.palette.type],
       borderRadius: theme.shape.borderRadius,
+      bottom: 0,
       content: "''",
       display: 'block',
-      margin: [[-theme.spacing(1), theme.spacing(2), -theme.spacing(1), 0]],
+      left: 0,
+      position: 'absolute',
+      top: 0,
       width: 2,
     },
   },
