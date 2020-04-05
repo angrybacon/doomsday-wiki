3cae000af089335f301949f9c001d1b8712b2c4e
Mathieu Marques <mathieumarques78@gmail.com>
2020-04-05 13:42:42 +0200

    Lazy tweets

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -2,6 +2,7 @@ import c from 'classnames';
 import E from 'exenv';
 import PropTypes from 'prop-types';
 import React, { useCallback, useEffect, useRef, useState  } from 'react';
+import { useInView } from 'react-intersection-observer';
 import Script from 'react-load-script';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { useTheme } from '@material-ui/core/styles';
@@ -11,12 +12,14 @@ import useStyles from './styles';
 
 export default function Tweet({ id }) {
 
-  const root = useRef(null);
-  const [ height, setHeight ] = useState(200);
+  const [ viewRoot, inView ] = useInView({rootMargin: '200px'});
+  const tweetRoot = useRef(null);
+  const [ height, setHeight ] = useState(0);
   const [ loading, setLoading ] = useState(true);
   const [ ready, setReady ] = useState(false);
   const [ should, setShould ] = useState(false);
   const [ update, setUpdate ] = useState(false);
+  const [ visible, setVisible ] = useState(false);
   const classes = useStyles();
   const theme = useTheme();
   const previousTheme = usePrevious(theme.palette.type);
@@ -24,7 +27,7 @@ export default function Tweet({ id }) {
   const onLoad = () => setReady(!!window.twttr);
 
   const onCreate = useCallback(() => {
-    window.twttr.widgets.createTweet(id, root.current, {
+    window.twttr.widgets.createTweet(id, tweetRoot.current, {
       align: 'center',
       conversation: 'none',
       dnt: true,
@@ -44,23 +47,38 @@ export default function Tweet({ id }) {
 
   useEffect(() => {
     if (previousTheme !== theme.palette.type) {
-      root.current.innerHTML = '';
+      if (tweetRoot.current) {
+        tweetRoot.current.innerHTML = '';
+      }
       setLoading(true);
       setUpdate(true);
     }
   }, [previousTheme, theme.palette.type]);
 
   useEffect(() => {
-    setShould(!!E.canUseDOM && id.length > 0);
-  }, [id]);
+    if (visible) {
+      setShould(!!E.canUseDOM && id.length > 0);
+    }
+  }, [id, visible]);
+
+  useEffect(() => {
+    if (!visible && inView) {
+      setHeight(200);
+      setVisible(true);
+    }
+  }, [inView, visible]);
 
   return (
-    <div className={classes.root} style={{height}}>
-      {should && <Script onLoad={onLoad} url="https://platform.twitter.com/widgets.js" />}
-      <div className={classes.container} ref={root} />
-      <div className={c(classes.loader, {[classes.loading]: loading})}>
-        {loading && <CircularProgress color="secondary" component="span" size={60} />}
-      </div>
+    <div className={classes.root} ref={viewRoot} style={{height}}>
+      {visible && (
+        <>
+          {should && <Script onLoad={onLoad} url="https://platform.twitter.com/widgets.js" />}
+          <div className={classes.container} ref={tweetRoot} />
+          <div className={c(classes.loader, {[classes.loading]: loading})}>
+            {loading && <CircularProgress color="secondary" component="span" size={60} />}
+          </div>
+        </>
+      )}
     </div>
   );
 }
