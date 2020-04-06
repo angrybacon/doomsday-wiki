ec8d526c2ef34e7ed26fba3a047684b22faca312
Mathieu Marques <mathieumarques78@gmail.com>
2020-04-06 22:23:05 +0200

    Swap welcome and recent articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import React from 'react';
 import Articles from '../components/Articles';
 import Paper from '../components/Paper';
 import Welcome from '../components/Welcome';
@@ -10,13 +10,13 @@ export default function PageHome() {
   return (
     <>
       <Box mb={1} ml={1}>
-        <Typography children="Recent Articles" color="textSecondary" variant="subtitle1" />
-      </Box>
-      <Box children={<Articles top />} mb={3} />
-      <Box mb={1} ml={1}>
         <Typography children="Welcome" color="textSecondary" variant="subtitle1" />
       </Box>
       <Box children={<Paper children={<Welcome />} />} mb={3} />
+      <Box mb={1} ml={1}>
+        <Typography children="Recent Articles" color="textSecondary" variant="subtitle1" />
+      </Box>
+      <Box children={<Articles top />} mb={3} />
     </>
   );
 }
