40850af9466a5a08a9c069443143b66a6d9baf6c
Mathieu Marques <mathieumarques78@gmail.com>
2020-04-14 13:32:48 +0200

    Automatically close drawer when changing route

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
@@ -6,6 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useLocation } from '@reach/router';
 import { SidebarContext } from '../../contexts/Sidebar';
 import Body from './Body';
 import Entries from './Entries';
@@ -14,10 +15,13 @@ import useStyles from './styles';
 
 
 export default function Sidebar() {
+
   const { drawer: drawerIsOpen, toggleDrawer } = useContext(SidebarContext);
+  const location = useLocation();
   const classes = useStyles();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.mixins.sidebar.treshold);
+
   const content = (
     <Grid container className={classes.root} direction="column" wrap="nowrap">
       <Grid item children={<Header />} />
@@ -28,6 +32,7 @@ export default function Sidebar() {
       </Grid>
     </Grid>
   );
+
   const drawer = (
     <SwipeableDrawer children={content}
                      classes={{paper: classes.drawerTemporary}}
@@ -35,8 +40,14 @@ export default function Sidebar() {
                      onOpen={toggleDrawer(true)}
                      open={drawerIsOpen} />
   );
+
   const drawerFixed = (
     <Drawer children={content} classes={{paper: classes.drawerFixed}} variant="permanent" />
   );
+
+  useEffect(() => {
+    toggleDrawer(false)();
+  }, [location, toggleDrawer]);
+
   return isMobile ? drawer : drawerFixed;
 }
--- src/contexts/Sidebar.js
+++ src/contexts/Sidebar.js
@@ -1,29 +1,14 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 
 export const SidebarContext = React.createContext();
 export function SidebarProvider({ children }) {
-
   const [ drawer, setDrawer ] = useState(false);
-  const [ sidebar, setSidebar ] = useState(true);
-
-  const toggleDrawer = open => () => {
+  const toggleDrawer = useCallback(open => () => {
     setDrawer(previous => open === undefined ? !previous.drawer : !!open);
-  };
-
-  const toggleSidebar = open => () => {
-    setSidebar(previous => open === undefined ? !previous.sidebar : !!open);
-  };
-
-  return (
-    <SidebarContext.Provider children={children} value={{
-      drawer,
-      sidebar,
-      toggleDrawer,
-      toggleSidebar,
-    }} />
-  );
+  }, []);
+  return <SidebarContext.Provider children={children} value={{drawer, toggleDrawer}} />;
 }
 
 
