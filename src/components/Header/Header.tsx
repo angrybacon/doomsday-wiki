d66313252a6d3a3b201b9200cc5c13be5f764eb6
Mathieu Marques <mathieumarques78@gmail.com>
2021-07-07 01:30:24 +0200

    fix(layout): fix layout offset

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,24 @@
 import React, { FunctionComponent } from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Slide from '@material-ui/core/Slide';
 import Toolbar from '@material-ui/core/Toolbar';
+import { Theme, makeStyles } from '@material-ui/core/styles';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  bar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+}));
+
 export const Header: FunctionComponent = () => {
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
+  const trigger = useScrollTrigger();
+  const classes = useStyles();
   return (
-    <AppBar elevation={trigger ? 4 : 0}>
-      <Toolbar>This is the header</Toolbar>
-    </AppBar>
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar className={classes.bar}>
+        <Toolbar>This is the header</Toolbar>
+      </AppBar>
+    </Slide>
   );
 };
