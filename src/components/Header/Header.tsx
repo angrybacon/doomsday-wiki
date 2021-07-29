5992af69cfff631dff5bd71eafe9869a6058a82d
Mathieu Marques <mathieumarques78@gmail.com>
2021-07-29 12:32:46 +0200

    feat(theme): enable server-rendered styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,21 +2,13 @@ import React, { FunctionComponent } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Slide from '@material-ui/core/Slide';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Theme, makeStyles } from '@material-ui/core/styles';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  bar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-}));
 
 export const Header: FunctionComponent = () => {
   const trigger = useScrollTrigger();
-  const classes = useStyles();
   return (
     <Slide appear={false} direction="down" in={!trigger}>
-      <AppBar className={classes.bar}>
+      <AppBar>
         <Toolbar>This is the header</Toolbar>
       </AppBar>
     </Slide>
