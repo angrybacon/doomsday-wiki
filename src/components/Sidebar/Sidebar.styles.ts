239e397a92349588fd06926160c94eaafa4d61d1
Mathieu Marques <mathieumarques78@gmail.com>
2021-08-14 22:59:14 +0200

    feat(header): add home button

--- src/components/Sidebar/Sidebar.styles.ts
+++ src/components/Sidebar/Sidebar.styles.ts
@@ -3,27 +3,18 @@ import type { Theme } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    header: {
+      ...theme.mixins.toolbar,
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+      [theme.breakpoints.up('sm')]: {
+        ...theme.mixins.toolbar,
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(2),
+      },
+    },
     paper: {
       width: theme.drawer.width,
     },
-    root: {
-      [theme.breakpoints.up('sm')]: {
-        width: theme.drawer.width,
-        flexShrink: 0,
-      },
-    },
-    appBar: {
-      [theme.breakpoints.up('sm')]: {
-        width: `calc(100% - ${theme.drawer.width}px)`,
-        marginLeft: theme.drawer.width,
-      },
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
-      },
-    },
-    toolbar: theme.mixins.toolbar,
   })
 );
