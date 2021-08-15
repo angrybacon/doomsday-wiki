f98b5edc0ee19749a2e4cfe881112b765af690a8
Mathieu Marques <mathieumarques78@gmail.com>
2021-08-15 20:52:26 +0200

    feat(chapters): add chapter pages

---
+++ src/pages/[category]/[chapter].tsx
@@ -0,0 +1,50 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { Layout } from '@/components/Layout/Layout';
+import { getChapters } from '@/tools/markdown/getChapters';
+import { getMarkdown } from '@/tools/markdown/getMarkdown';
+import type { Document, Markdown, WithMenu } from '@/tools/markdown/types';
+
+interface Props {
+  markdown: Markdown;
+}
+
+const ChapterPage: NextPage<Props & WithMenu> = ({ markdown, menu }) => (
+  <Layout maxWidth="md" menu={menu} title={markdown?.data?.title}>
+    <Card>
+      <CardContent component={ReactMarkdown}>{markdown.content}</CardContent>
+    </Card>
+  </Layout>
+);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const chapters: Document[] = getChapters();
+  const paths = chapters.map(({ crumbs }) => {
+    const [category, chapter] = crumbs;
+    return { params: { category, chapter } };
+  });
+  return { fallback: false, paths };
+};
+
+interface Query extends ParsedUrlQuery {
+  category: string;
+  chapter: string;
+}
+
+export const getStaticProps: GetStaticProps<Props, Query> = async ({
+  params,
+}) => ({
+  props: {
+    markdown: getMarkdown(
+      'chapters',
+      params?.category ?? '',
+      params?.chapter ?? ''
+    ),
+  },
+});
+
+export default ChapterPage;
