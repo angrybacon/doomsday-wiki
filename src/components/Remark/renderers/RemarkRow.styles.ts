2b63707fd185faee8b1399e091bfff603157d2d0
Mathieu Marques <mathieumarques78@gmail.com>
2021-08-29 21:40:14 +0200

    refactor(theme): add gutters and barf mixins

--- src/components/Remark/renderers/RemarkRow.styles.ts
+++ src/components/Remark/renderers/RemarkRow.styles.ts
@@ -1,6 +1,5 @@
 import { alpha, createStyles, makeStyles } from '@material-ui/core/styles';
 import type { Theme } from '@material-ui/core/styles';
-import type { CSSProperties } from '@material-ui/core/styles/withStyles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,18 +17,13 @@ export const useStyles = makeStyles((theme: Theme) =>
     },
 
     pile: {
+      ...theme.mixins.barf(),
       backgroundColor: alpha(theme.palette.primary.main, 0.2),
       borderColor: theme.palette.divider,
       borderLeftWidth: 0,
       borderRightWidth: 0,
       borderStyle: 'solid',
       borderWidth: 1,
-      marginLeft: -(
-        (theme.overrides?.MuiCardContent?.root as CSSProperties)?.padding ?? 0
-      ),
-      marginRight: -(
-        (theme.overrides?.MuiCardContent?.root as CSSProperties)?.padding ?? 0
-      ),
       padding: theme.spacing(2),
 
       '& $card': {
