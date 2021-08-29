cb65b1d523060fbec32296d9e2b90141aba405fa
Mathieu Marques <mathieumarques78@gmail.com>
2021-08-29 03:01:10 +0200

    refactor(scryfall): retrieve oldest print by default
    - Provide default sets
    - Provide name shorthands

---
+++ src/tools/scryfall/cards.ts
@@ -0,0 +1,26 @@
+/** List of default preferred sets for given card names. */
+export const SETS: Record<string, string> =
+  // prettier-ignore
+  {
+  'Brainstorm':        'MMQ',
+  'Doomsday':          'WTH',
+  'Infernal Contract': 'MIR',
+  'Echo of Eons':      'MH1',
+  "Thassa's Oracle":   'THB',
+};
+
+/** Commonly used acronyms. */
+export const SHORTHANDS: Record<string, string> =
+  // prettier-ignore
+  {
+  BS:    'Brainstorm',
+  DD:    'Doomsday',
+  EoE:   'Echo of Eons',
+  EtW:   'Empty the Warrens',
+  IC:    'Infernal Contract',
+  LDV:   "Lim-Dul's Vault",
+  Mm:    'Manamorphose',
+  SotV:  'Spoils of the Vault',
+  TO:    "Thassa's Oracle",
+  ToA:   'Tendrils of Agony',
+};
--- src/tools/scryfall/scry.ts
+++ src/tools/scryfall/scry.ts
@@ -1,42 +1,69 @@
 import axios, { AxiosResponse } from 'axios';
 import { HttpStatusCode } from '@/constants/HttpStatusCode';
+import { SETS, SHORTHANDS } from '@/tools/scryfall/cards';
+
+type Cache = Record<string, AxiosResponse | Promise<AxiosResponse>>;
 
 /**
  * Cache to hold concurrent search queries.
  * The content can either be a resolved or rejected promise, or a pending
  * promise.
  */
-const CACHE: Record<string, AxiosResponse | Promise<AxiosResponse>> = {};
+const CACHE: { named: Cache; search: Cache } = {
+  named: {},
+  search: {},
+};
 
-/** Endpoint to query for a card name and optional set. */
-const SCRYFALL_API = 'https://api.scryfall.com/cards/named';
+/** Endpoints to query for a card name. */
+enum Api {
+  NAMED = 'https://api.scryfall.com/cards/named',
+  SEARCH = 'https://api.scryfall.com/cards/search',
+}
+
+/**
+ * Return a tuple representing the Scryfall configuration to use depending on
+ * whether the query is ambiguous or definite.
+ */
+const getApi = (name: string, set?: string): [Cache, Api, string] =>
+  set
+    ? // NOTE If a set is specified, the search will not be ambiguous
+      [CACHE.named, Api.NAMED, `exact=${name}&set=${set}`]
+    : // NOTE Else, use the standard search and consider the first result only
+      [
+        CACHE.search,
+        Api.SEARCH,
+        `q=!"${name}"&unique=prints&order=released&dir=asc`,
+      ];
 
 type Scry = (key: string, set?: string) => Promise<AxiosResponse>;
 
 /**
- * Query api.scryfall.com for `name` in `set`.
+ * Query Scryfall for `name` in `set`.
  * Build a cache to avoid querying twice for the same name and set pair.
  */
 export const scry: Scry = (name, set = '') =>
   new Promise<AxiosResponse>((resolve, reject) => {
-    const key = `${name}/${set}`;
-    if (CACHE[key]) {
+    const realName = SHORTHANDS[name] || name;
+    const realSet = set || SETS[realName];
+    const [cache, api, query] = getApi(realName, realSet);
+    const key = `${realName}/${realSet}`;
+    if (cache[key]) {
       if (
-        (CACHE[key] as AxiosResponse).status === undefined ||
-        (CACHE[key] as AxiosResponse).status === HttpStatusCode.OK
+        (cache[key] as AxiosResponse).status === undefined ||
+        (cache[key] as AxiosResponse).status === HttpStatusCode.OK
       ) {
-        return resolve(CACHE[key]);
+        return resolve(cache[key]);
       }
-      return reject(CACHE[key]);
+      return reject(cache[key]);
     }
-    CACHE[key] = axios.get(`${SCRYFALL_API}?exact=${name}&set=${set}`);
-    return (CACHE[key] as Promise<AxiosResponse>).then(
+    cache[key] = axios.get(`${api}?${query}`);
+    return (cache[key] as Promise<AxiosResponse>).then(
       (response) => {
-        CACHE[key] = response;
+        cache[key] = response;
         return resolve(response);
       },
       (error) => {
-        CACHE[key] = error.response;
+        cache[key] = error.response;
         return reject(error.response);
       }
     );
