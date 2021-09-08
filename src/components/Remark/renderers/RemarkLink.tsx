6566df1325136fb91c004cf6523e55bf6cffab97
Mathieu Marques <mathieumarques78@gmail.com>
2021-09-08 13:13:51 +0200

    feat(link): prefer external for absolute paths

--- src/components/Remark/renderers/RemarkLink.tsx
+++ src/components/Remark/renderers/RemarkLink.tsx
@@ -2,5 +2,13 @@ import React from 'react';
 import type { Components } from 'react-markdown';
 import { Link } from '@/components/Link/Link';
 
-export const RemarkLink: Components['a'] = ({ children, href }) =>
-  href ? <Link href={href}>{children}</Link> : { children };
+export const RemarkLink: Components['a'] = ({ children, href }) => {
+  if (!href) {
+    return children;
+  }
+  return (
+    <Link external={href.startsWith('http')} href={href}>
+      {children}
+    </Link>
+  );
+};
