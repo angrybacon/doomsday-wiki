057bdacf0c20285746f8760928e55b6e827609f8
Mathieu Marques <mathieumarques78@gmail.com>
2021-09-10 14:58:10 +0100

    test(decklist): add column tests

--- src/components/Decklist/Column.tsx
+++ src/components/Decklist/Column.tsx
@@ -3,20 +3,27 @@ import Typography from '@material-ui/core/Typography';
 import type { Card } from '@/tools/decklists/types';
 import { useStyles } from './Column.styles';
 
-interface Props {
+export interface Props {
   cards: Card[];
 }
 
 export const Column: FunctionComponent<Props> = ({ cards }) => {
   const classes = useStyles();
+  if (!cards.length) return null;
   return (
     <ul className={classes.root}>
       {cards.map(([quantity, name]) => (
-        <li className={classes.line} key={`column-card-${quantity}-${name}`}>
-          <Typography className={classes.quantity} variant="body2">
+        <li className={classes.line} key={`card-${quantity}-${name}`}>
+          <Typography
+            className={classes.quantity}
+            component="span"
+            variant="body2"
+          >
             {quantity}
           </Typography>
-          <Typography variant="body2">{name}</Typography>
+          <Typography component="span" variant="body2">
+            {name}
+          </Typography>
         </li>
       ))}
     </ul>
