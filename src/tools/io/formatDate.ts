2f1b7a3a1d97b6f1fdfec313c9d84297f40de47c
Mathieu Marques <mathieumarques78@gmail.com>
2021-09-11 17:18:17 +0100

    feat(home): prettify article dates

---
+++ src/tools/io/formatDate.ts
@@ -0,0 +1,28 @@
+type FormatDate = (
+  year?: string,
+  month?: string,
+  day?: string
+) => null | string;
+
+/**
+ * Parse a date out of an array representing the year, month and day in that
+ * order.
+ * Accept partial dates where day or even month can be omitted.
+ * Return a local-formatted string representing the date found. Otherwise return
+ * null.
+ */
+export const formatDate: FormatDate = (year, month, day) => {
+  if (!year) return null;
+  // NOTE Replace missing parts with the first month or day to still have a
+  //      valid date object.
+  const date = new Date(
+    parseInt(year, 10),
+    month ? parseInt(month, 10) - 1 : 0,
+    day ? parseInt(day, 10) : 1
+  );
+  return date.toLocaleDateString(undefined, {
+    year: 'numeric',
+    ...(month && { month: 'long' }),
+    ...(day && { day: 'numeric' }),
+  });
+};
