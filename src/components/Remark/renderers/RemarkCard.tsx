c29847e939f4a05704217c6865275a37fefd5b32
Mathieu Marques <mathieumarques78@gmail.com>
2021-09-26 18:00:09 +0200

    refactor(remark): substitute card shorthands server-side

--- src/components/Remark/renderers/RemarkCard.tsx
+++ src/components/Remark/renderers/RemarkCard.tsx
@@ -2,20 +2,15 @@ import NextLink from 'next/link';
 import React, { FunctionComponent } from 'react';
 import type { ReactMarkdownProps } from 'react-markdown/lib/ast-to-react';
 import MuiLink from '@material-ui/core/Link';
-import { SHORTHANDS } from '@/tools/scryfall/cards';
 
 const SCRYFALL_SEARCH = 'https://scryfall.com/search';
 
 export interface Props extends ReactMarkdownProps {
-  node: ReactMarkdownProps['node'] & {
-    properties: { name?: string };
-  };
+  name?: string;
 }
 
-export const RemarkCard: FunctionComponent<Props> = ({ node }) => {
-  let { name } = node.properties;
+export const RemarkCard: FunctionComponent<Props> = ({ name }) => {
   if (!name) return null;
-  name = SHORTHANDS[name] || name;
   return (
     <NextLink href={`${SCRYFALL_SEARCH}?q=${name}`} passHref>
       <MuiLink rel="noopener noreferrer" target="_blank">
--- src/tools/remark/remarkCard.ts
+++ src/tools/remark/remarkCard.ts
@@ -1,8 +1,9 @@
 import type { Text } from 'mdast';
-import type { Plugin } from 'unified';
 import type { TextDirective } from 'mdast-util-directive';
+import type { Plugin } from 'unified';
 import { select } from 'unist-util-select';
 import { Node, Test, visit } from 'unist-util-visit';
+import { SHORTHANDS } from '@/tools/scryfall/cards';
 
 /** Parse card directives and augment properties with the card name. */
 export const remarkCard: Plugin = () => (tree) => {
@@ -15,7 +16,7 @@ export const remarkCard: Plugin = () => (tree) => {
         ...directive.data,
         hProperties: {
           ...(directive.data?.hProperties as Record<string, unknown>),
-          name: text.value,
+          name: SHORTHANDS[text.value] || text.value,
         },
       };
     }
