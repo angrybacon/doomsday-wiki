98f438baf69ab786f110c3ca89daeea501323909
Mathieu Marques <mathieumarques78@gmail.com>
2021-09-27 01:48:01 +0200

    fix(remark): fix typings

--- src/components/Remark/renderers/RemarkLink.tsx
+++ src/components/Remark/renderers/RemarkLink.tsx
@@ -4,7 +4,7 @@ import { Link } from '@/components/Link/Link';
 
 export const RemarkLink: Components['a'] = ({ children, href }) => {
   if (!href) {
-    return children;
+    return <>{children}</>;
   }
   return (
     <Link external={href.startsWith('http')} href={href}>
