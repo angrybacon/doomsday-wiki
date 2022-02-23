54af489d0282f01e01b0f91eeb4c03c8f64c213c
Mathieu Marques <mathieumarques78@gmail.com>
2022-02-23 12:21:42 +0100

    fix(remark): remove margin reset from default dividers

--- src/components/Remark/renderers/RemarkDivider.styles.ts
+++ src/components/Remark/renderers/RemarkDivider.styles.ts
@@ -3,6 +3,13 @@ import { createStyles, makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: theme.mixins.barf,
+    root: {
+      ...theme.mixins.barf,
+      // NOTE Increase specificity
+      '&&': {
+        marginBottom: theme.spacing(3),
+        marginTop: theme.spacing(3),
+      },
+    },
   })
 );
