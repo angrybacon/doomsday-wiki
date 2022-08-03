09ebe9da5089e7287da9adefeaf9971370e5839d
Mathieu Marques <mathieumarques78@gmail.com>
2022-08-03 16:22:01 +0200

    build(next): prefer SWC over Babel (#177)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+module.exports = nextJest()({
   moduleNameMapper: { '^@/(.+)$': '<rootDir>/src/$1' },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jsdom',
-};
+  testEnvironment: 'jest-environment-jsdom',
+});
