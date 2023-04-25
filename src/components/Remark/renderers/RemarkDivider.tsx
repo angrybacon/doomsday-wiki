fe6a506090f3a5d45006f74eec6a5890efef040f
Mathieu Marques <mathieumarques78@gmail.com>
2023-04-25 03:06:52 +0200

    fix(remark): remove divider margins

--- src/components/Remark/renderers/RemarkDivider.tsx
+++ src/components/Remark/renderers/RemarkDivider.tsx
@@ -2,10 +2,5 @@ import { FunctionComponent } from 'react';
 import Divider from '@mui/material/Divider';
 
 export const RemarkDivider: FunctionComponent = () => (
-  <Divider
-    sx={({ mixins, spacing }) => ({
-      ...mixins.barf,
-      my: `${spacing(3)}!important`,
-    })}
-  />
+  <Divider sx={({ mixins }) => ({ ...mixins.barf, my: 0 })} />
 );
