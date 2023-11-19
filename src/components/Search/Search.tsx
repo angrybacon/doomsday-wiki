3fbaa1dff6c569dda8cb1d49b6008c74cfe7e5a7
Mathieu Marques <mathieumarques78@gmail.com>
2023-11-19 23:05:54 +0100

    feat(search): add global hotkey

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,17 +1,30 @@
 import {
   useEffect,
+  useRef,
   useState,
   type ChangeEvent,
   type FunctionComponent,
 } from 'react';
-import { InputBase } from '@mui/material';
+import { useHotkeys } from 'react-hotkeys-hook';
+import { InputBase, Typography } from '@mui/material';
 import { useDebounce } from '@/hooks/useDebounce';
 
 export const Search: FunctionComponent = () => {
+  const root = useRef<HTMLInputElement>(null);
   const [input, setInput] = useState('');
   const query = useDebounce(input, 200);
 
   useEffect(() => {}, [query]);
+
+  useHotkeys(
+    '/',
+    (event) => {
+      event.preventDefault(); // NOTE Prevents typing the `/`
+      root.current?.focus();
+      root.current?.select();
+    },
+    [root.current],
+  );
 
   const onChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
     setInput(target.value);
@@ -20,9 +33,26 @@ export const Search: FunctionComponent = () => {
   return (
     <InputBase
       fullWidth
-      inputProps={{ 'aria-label': 'search' }}
+      inputProps={{ 'aria-label': 'search', ref: root }}
       onChange={onChange}
       placeholder="Search"
+      startAdornment={
+        <Typography
+          sx={{
+            backgroundColor: 'action.focus',
+            border: 1,
+            borderColor: 'divider',
+            borderRadius: '4px',
+            color: 'text.secondary',
+            fontFamily: 'monospace',
+            mr: 1,
+            px: 0.5,
+          }}
+          variant="caption"
+        >
+          /
+        </Typography>
+      }
       value={input}
     />
   );
