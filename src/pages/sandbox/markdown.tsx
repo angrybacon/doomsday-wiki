9e24f01fb97e80bdcedb12b4f76351f7e9092b7a
Mathieu Marques <mathieumarques78@gmail.com>
2024-03-10 00:10:51 +0100

    feat(sandbox): add sandbox/markdown page

---
+++ src/pages/sandbox/markdown.tsx
@@ -0,0 +1,36 @@
+import { Card, CardContent } from '@mui/material';
+import { GetStaticProps, NextPage } from 'next';
+
+import { Layout } from '@/components/Layout/Layout';
+import { Remark } from '@/components/Remark/Remark';
+import { getDecklists } from '@/tools/decklists/getDecklists';
+import { Decklists } from '@/tools/decklists/types';
+import { getPartial } from '@/tools/markdown/getMarkdown';
+import { getMenu } from '@/tools/markdown/getMenu';
+import { Partial, type MenuEntry } from '@/tools/markdown/types';
+
+interface Props {
+  decklists: Decklists;
+  markdown: Partial;
+  menu: MenuEntry[];
+}
+
+const Page: NextPage<Props> = ({ decklists, markdown, menu }) => (
+  <Layout menu={menu} title="Sandbox">
+    <Card>
+      <CardContent>
+        <Remark decklists={decklists} markdown={markdown} />
+      </CardContent>
+    </Card>
+  </Layout>
+);
+
+export const getStaticProps: GetStaticProps<Props> = async () => ({
+  props: {
+    decklists: getDecklists(),
+    markdown: await getPartial('sandbox'),
+    menu: getMenu(),
+  },
+});
+
+export default Page;
