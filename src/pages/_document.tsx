165f34a0b09a4fc72101df86fb41901df997ef65
Mathieu Marques <mathieumarques78@gmail.com>
2024-05-26 13:27:36 +0200

    refactor(typescript): use korumite/ts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,17 @@
-import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 import { darkTheme as theme } from '@/theme/theme';
 
-export default class Document extends NextDocument {
-  render(): JSX.Element {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta content={theme.palette.primary.main} name="theme-color" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta content={theme.palette.primary.main} name="theme-color" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
