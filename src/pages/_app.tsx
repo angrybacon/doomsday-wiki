3e447e79ca94dbcc6f1869980f2d8e0036be2534
Mathieu Marques <mathieumarques78@gmail.com>
2024-09-29 02:14:37 +0200

    feat(tracking): add PostHog (#253)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import { CssBaseline } from '@mui/material';
 import { Analytics } from '@vercel/analytics/react';
 import { type AppProps } from 'next/app';
 import Head from 'next/head';
+import postHog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 import { ThemeProvider } from '@/theme/ThemeContext';
 
-export default function Application(props: AppProps) {
-  const { Component, pageProps } = props;
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  postHog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'always',
+  });
+}
+
+export default function Application({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
@@ -18,8 +26,9 @@ export default function Application(props: AppProps) {
       </Head>
       <ThemeProvider>
         <CssBaseline />
-        {/* TODO Provide decklists and menu through a shared context */}
-        <Component {...pageProps} />
+        <PostHogProvider client={postHog}>
+          <Component {...pageProps} />
+        </PostHogProvider>
       </ThemeProvider>
       {process.env.NODE_ENV === 'production' && <Analytics />}
     </>
